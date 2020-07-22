9a5d7e09d859594b19bb73694881e6195484fcf4
thaihuynhxyz <thaihuynhxyz@gmail.com>
2020-07-22 23:09:42 +0700

    Create Flutter Project

---
+++ example/android/app/src/main/java/org/demen/speech_commands_example/MainActivity.java
@@ -0,0 +1,6 @@
+package org.demen.speech_commands_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/SpeechCommandsPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SpeechCommandsPlugin : NSObject<FlutterPlugin>
+@end
